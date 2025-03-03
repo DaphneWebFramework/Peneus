1449cc91324b02a73cf1d5f3fd258a9b0ba95e12
eylemugurel <eylemugurel@hotmail.com>
2025-03-03 08:16:44 +0100

    CsrfToken: Add docblocks

--- Services/Model/CsrfToken.php
+++ Services/Model/CsrfToken.php
@@ -12,22 +12,45 @@
 
 namespace Peneus\Services\Model;
 
+/**
+ * Represents a CSRF token and its hashed counterpart stored in a cookie.
+ */
 class CsrfToken
 {
     private readonly string $token;
     private readonly string $cookieValue;
 
+    /**
+     * Creates a new instance.
+     *
+     * @param string $token
+     *   The token value.
+     * @param string $cookieValue
+     *   The token hash stored in a cookie.
+     */
     public function __construct(string $token, string $cookieValue)
     {
         $this->token = $token;
         $this->cookieValue = $cookieValue;
     }
 
+    /**
+     * Gets the token value.
+     *
+     * @return string
+     *   The token value.
+     */
     public function Token(): string
     {
         return $this->token;
     }
 
+    /**
+     * Gets the token hash stored in a cookie.
+     *
+     * @return string
+     *   The token hash.
+     */
     public function CookieValue(): string
     {
         return $this->cookieValue;

9c2f18cf1457eaa992158a7121ec49aeebccbf8b
eylemugurel <eylemugurel@hotmail.com>
2025-03-22 15:46:38 +0100

    PageSystem: Add Page and Renderer classes (MVP)

---
+++ Systems/PageSystem/Page.php
@@ -0,0 +1,217 @@
+<?php declare(strict_types=1);
+/**
+ * Page.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Peneus\Systems\PageSystem;
+
+use \Harmonia\Config;
+
+/**
+ * Represents a web page and manages its basic properties and rendering flow.
+ *
+ * #### Example
+ * ```php
+ * require '../../bootstrap.php';
+ *
+ * use \Peneus\Systems\PageSystem\Page;
+ *
+ * <?php
+ * $page = (new Page)
+ *     ->SetTitle('Home')
+ *     ->SetMasterPage('basic');
+ * ?>
+ *
+ * <?php $page->Begin()?>
+ *     <h1>Welcome to my website</h1>
+ *     <p>This is the home page.</p>
+ * <?php $page->End()?>
+ * ```
+ */
+class Page
+{
+    private readonly Renderer $renderer;
+
+    private string $title = '';
+    private string $titleTemplate = '{{Title}} | {{AppName}}';
+    private string $masterpage = '';
+    private string $contents = '';
+
+    #region public -------------------------------------------------------------
+
+    /**
+     * Constructs a new instance.
+     *
+     * @param ?Renderer $renderer
+     *   (Optional) The renderer to use. If not specified, a default instance is
+     *   created.
+     */
+    public function __construct(?Renderer $renderer = null)
+    {
+        $this->renderer = $renderer ?? new Renderer();
+    }
+
+    #region setters ------------------------------------------------------------
+
+    /**
+     * Sets the page title.
+     *
+     * @param string $title
+     *   The title of the page. It will be substituted into the title template
+     *   when the final title is retrieved.
+     * @return self
+     *   The current instance.
+     *
+     * @see SetTitleTemplate
+     * @see Title
+     */
+    public function SetTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Sets the template used to generate the final page title.
+     *
+     * If not specified, the default template `{{Title}} | {{AppName}}` is used.
+     *
+     * @param string $titleTemplate
+     *   A template string that may include placeholders such as `{{Title}}` and
+     *   `{{AppName}}`.
+     * @return self
+     *   The current instance.
+     *
+     * @see SetTitle
+     * @see Title
+     */
+    public function SetTitleTemplate(string $titleTemplate): self
+    {
+        $this->titleTemplate = $titleTemplate;
+        return $this;
+    }
+
+    /**
+     * Sets the masterpage used to layout the page content.
+     *
+     * @param string $masterpage
+     *   The file name (without extension) of a masterpage under the
+     *   `masterpages` directory (e.g. `basic`, `starter`).
+     * @return self
+     *   The current instance.
+     */
+    public function SetMasterpage(string $masterpage): self
+    {
+        $this->masterpage = $masterpage;
+        return $this;
+    }
+
+    #endregion setters
+
+    #region getters ------------------------------------------------------------
+
+    /**
+     * Returns the generated page title.
+     *
+     * The returned string is produced by substituting the title (set via
+     * `SetTitle`) and the application name (retrieved from configuration)
+     * into the title template (set via `SetTitleTemplate`).
+     *
+     * If the application name is empty, only the title is returned.
+     * If the title is empty, only the application name is returned.
+     *
+     * @return string
+     *   The generated title string.
+     *
+     * @see SetTitle
+     * @see SetTitleTemplate
+     */
+    public function Title(): string
+    {
+        $appName = Config::Instance()->OptionOrDefault('AppName', '');
+        if ($appName === '') {
+            return $this->title;
+        }
+        if ($this->title === '') {
+            return $appName;
+        }
+        return \strtr($this->titleTemplate, [
+            '{{Title}}' => $this->title,
+            '{{AppName}}' => $appName,
+        ]);
+    }
+
+    /**
+     * Returns the selected masterpage name.
+     *
+     * @return string
+     *   The masterpage name.
+     *
+     * @see SetMasterpage
+     */
+    public function Masterpage(): string
+    {
+        return $this->masterpage;
+    }
+
+    /**
+     * Returns the captured page content.
+     *
+     * @return string
+     *   The content between calls to `Begin()` and `End()`.
+     */
+    public function Contents(): string
+    {
+        return $this->contents;
+    }
+
+    #endregion getters
+
+    #region operations ---------------------------------------------------------
+
+    /**
+     * Begins capturing the page content using output buffering.
+     */
+    public function Begin(): void
+    {
+        $this->contents = '';
+        $this->_ob_start();
+    }
+
+    /**
+     * Ends content capture and renders the page.
+     */
+    public function End(): void
+    {
+        $this->contents = $this->_ob_get_clean();
+        $this->renderer->Render($this);
+    }
+
+    #endregion operations
+
+    #endregion public
+
+    #region protected ----------------------------------------------------------
+
+    /** @codeCoverageIgnore */
+    protected function _ob_start(): void
+    {
+        \ob_start();
+    }
+
+    /** @codeCoverageIgnore */
+    protected function _ob_get_clean(): string
+    {
+        $contents = \ob_get_clean();
+        return $contents === false ? '' : $contents;
+    }
+
+    #endregion protected
+}
---
+++ Systems/PageSystem/Renderer.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types=1);
+/**
+ * Renderer.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Peneus\Systems\PageSystem;
+
+use \Harmonia\Config;
+use \Harmonia\Core\CFile;
+use \Harmonia\Core\CPath;
+use \Harmonia\Logger;
+use \Peneus\Resource;
+
+/**
+ * Handles the rendering of a web page using a template and an optional
+ * masterpage.
+ */
+class Renderer
+{
+    /**
+     * Renders the complete page output.
+     *
+     * Loads the base page template, replaces placeholders, and includes the
+     * masterpage if one is configured. The final result is sent to output.
+     *
+     * @param Page $page
+     *   The page instance to render.
+     */
+    public function Render(Page $page): void
+    {
+        $file = $this->openFile(Resource::Instance()->TemplateFilePath('page'));
+        if ($file === null) {
+            Logger::Instance()->Error('Page template not found.');
+            return;
+        }
+        $template = $file->Read();
+        $file->Close();
+        if ($template === null) {
+            Logger::Instance()->Error('Page template could not be read.');
+            return;
+        }
+        $html = \strtr($template, [
+            '{{Language}}' => Config::Instance()->OptionOrDefault('Language', ''),
+            '{{Title}}' => $page->Title(),
+            "\t{{Contents}}" => $this->masterContents($page),
+        ]);
+        $this->_echo($html);
+    }
+
+    #region protected ----------------------------------------------------------
+
+    /**
+     * Generates the contents section by executing the selected masterpage.
+     *
+     * If a masterpage is defined, it is included with `$this` bound to the
+     * page instance, allowing access to its methods and properties. Otherwise,
+     * the raw page content is returned as-is.
+     *
+     * @param Page $page
+     *   The page instance providing the content and masterpage name.
+     * @return string
+     *   The generated contents section to be inserted into the template.
+     */
+    protected function masterContents(Page $page): string
+    {
+        $this->_ob_start();
+        $masterpage = $page->Masterpage();
+        if ($masterpage === '') {
+            $this->_echo($page->Contents());
+        } else {
+            $masterpagePath = Resource::Instance()->MasterpageFilePath($masterpage);
+            if (!$masterpagePath->IsFile()) {
+                Logger::Instance()->Error("Masterpage not found: {$masterpage}");
+            } else {
+                // Render the masterpage within the context of the Page instance,
+                // so that `$this` inside the masterpage refers to Page and its
+                // methods can be accessed.
+                (function() use($masterpagePath) {
+                    include $masterpagePath;
+                })->call($page);
+            }
+        }
+        return $this->_ob_get_clean();
+    }
+
+    /** @codeCoverageIgnore */
+    protected function _ob_start(): void
+    {
+        \ob_start();
+    }
+
+    /** @codeCoverageIgnore */
+    protected function _ob_get_clean(): string
+    {
+        $contents = \ob_get_clean();
+        return $contents === false ? '' : $contents;
+    }
+
+    /** @codeCoverageIgnore */
+    protected function _echo(string $string): void
+    {
+        echo $string;
+    }
+
+    /** @codeCoverageIgnore */
+    protected function openFile(CPath $filePath): ?CFile
+    {
+        return CFile::Open($filePath);
+    }
+
+    #endregion protected
+}

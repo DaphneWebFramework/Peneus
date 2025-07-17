f5080c4091c15583c6f34ec48f28b0c5fa895e1f
eylemugurel <eylemugurel@hotmail.com>
2025-07-17 16:00:04 +0200

    Api: Add AddRecordAction and ModelValidationRulesProvider

---
+++ Api/Actions/Management/AddRecordAction.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+/**
+ * AddRecordAction.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Peneus\Api\Actions\Management;
+
+use \Peneus\Api\Actions\Action;
+
+use \Harmonia\Http\Request;
+use \Harmonia\Systems\ValidationSystem\Validator;
+use \Peneus\Model\Entity;
+
+/**
+ * Creates and persists a new record into a specified table.
+ */
+class AddRecordAction extends Action
+{
+    use ModelClassResolver;
+    use ModelValidationRulesProvider;
+
+    /**
+     * Executes the process of adding a new record to a specified table.
+     *
+     * Validates the table name from the query parameters and determines
+     * the corresponding model class. Then validates the request body
+     * according to model-specific rules, constructs a new model instance
+     * from the validated fields, persists it, and returns the identifier
+     * of the newly inserted record.
+     *
+     * @return array<string, int>
+     *   An associative array containing the key 'id' mapped to the primary
+     *   key of the newly inserted record.
+     * @throws \InvalidArgumentException
+     *   If the table name is not recognized or the request body fails validation.
+     * @throws \RuntimeException
+     *   If the record cannot be saved to the data store.
+     */
+    protected function onExecute(): mixed
+    {
+        $validator = new Validator([ 'table' => ['required', 'string'] ]);
+        $dataAccessor = $validator->Validate(Request::Instance()->QueryParams());
+        $table = $dataAccessor->GetField('table');
+
+        $modelClass = $this->resolveModelClass($table);
+
+        $validator = new Validator($this->validationRulesForAdd($modelClass));
+        $dataAccessor = $validator->Validate(Request::Instance()->JsonBody());
+
+        $entity = $this->createEntity($modelClass, $dataAccessor->Data());
+        if (!$entity->Save()) {
+            throw new \RuntimeException("Failed to add record to table '$table'.");
+        }
+        return [ 'id' => $entity->id ];
+    }
+
+    /**
+     * @param class-string $modelClass
+     * @param array<string, mixed> $data
+     * @return Entity
+     * 
+     * @codeCoverageIgnore
+     */
+    protected function createEntity(string $modelClass, array $data): Entity
+    {
+        return new $modelClass($data);
+    }
+}
---
+++ Api/Actions/Management/ModelValidationRulesProvider.php
@@ -0,0 +1,151 @@
+<?php declare(strict_types=1);
+/**
+ * ModelValidationRulesProvider.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Peneus\Api\Actions\Management;
+
+use \Harmonia\Services\SecurityService;
+use \Peneus\Model\Account;
+use \Peneus\Model\AccountRole;
+use \Peneus\Model\PasswordReset;
+use \Peneus\Model\PendingAccount;
+use \Peneus\Model\Role;
+use \Peneus\Services\AccountService;
+
+/**
+ * Provides validation rules for model-specific create/edit operations.
+ */
+trait ModelValidationRulesProvider
+{
+    /**
+     * Returns the validation rule set for creating a new record of the
+     * given model class.
+     *
+     * @param class-string $modelClass
+     *   Fully qualified model class name.
+     * @return array<string, array<int, string>>
+     *   Field names mapped to rule arrays.
+     * @throws \InvalidArgumentException
+     *   If the given model class is not supported.
+     */
+    protected function validationRulesForAdd(string $modelClass): array
+    {
+        return match ($modelClass) {
+            Account::class => [
+                'email' => [
+                    'required',
+                    'email'
+                ],
+                'passwordHash' => [
+                    'required',
+                    'regex:' . SecurityService::PASSWORD_HASH_PATTERN
+                ],
+                'displayName' => [
+                    'required',
+                    'regex:' . AccountService::DISPLAY_NAME_PATTERN
+                ],
+                'timeActivated' => [
+                    'required',
+                    'datetime:Y-m-d H:i:s'
+                ],
+                'timeLastLogin' => [
+                    'required',
+                    'nullable',
+                    'datetime:Y-m-d H:i:s'
+                ],
+            ],
+            AccountRole::class => [
+                'accountId' => [
+                    'required',
+                    'integer',
+                    'min:1'
+                ],
+                'role' => [
+                    'required',
+                    'integer',
+                    function($value) {
+                        return null !== Role::tryFrom($value);
+                    }
+                ],
+            ],
+            PendingAccount::class => [
+                'email' => [
+                    'required',
+                    'email'
+                ],
+                'passwordHash' => [
+                    'required',
+                    'regex:' . SecurityService::PASSWORD_HASH_PATTERN
+                ],
+                'displayName' => [
+                    'required',
+                    'regex:' . AccountService::DISPLAY_NAME_PATTERN
+                ],
+                'activationCode' => [
+                    'required',
+                    'regex:' . SecurityService::TOKEN_PATTERN
+                ],
+                'timeRegistered' => [
+                    'required',
+                    'datetime:Y-m-d H:i:s'
+                ],
+            ],
+            PasswordReset::class => [
+                'accountId' => [
+                    'required',
+                    'integer',
+                    'min:1'
+                ],
+                'resetCode' => [
+                    'required',
+                    'regex:' . SecurityService::TOKEN_PATTERN
+                ],
+                'timeRequested' => [
+                    'required',
+                    'datetime:Y-m-d H:i:s'
+                ],
+            ],
+            default => throw new \InvalidArgumentException(
+                "Unsupported model class: $modelClass"),
+        };
+    }
+
+    /**
+     * Returns the validation rule set for deleting a record of the given model
+     * class.
+     *
+     * @return array<string, array<int, string>>
+     *   The 'id' field name mapped to an array of rules.
+     */
+    protected function validationRulesForDelete(): array
+    {
+        return [ 'id' => ['required', 'integer', 'min:1'] ];
+    }
+
+    /**
+     * Returns the validation rule set for editing an existing record of the
+     * given model class.
+     *
+     * @param class-string $modelClass
+     *   Fully qualified model class name.
+     * @return array<string, array<int, string>>
+     *   Field names mapped to rule arrays.
+     * @throws \InvalidArgumentException
+     *   If the given model class is not supported.
+      */
+    protected function validationRulesForEdit(string $modelClass): array
+    {
+        return array_merge(
+            $this->validationRulesForDelete(),
+            $this->validationRulesForAdd($modelClass)
+        );
+    }
+}

f817828463fcc8da6f622439fe827f6fa4c24267
eylemugurel <eylemugurel@hotmail.com>
2025-08-04 05:35:04 +0200

    Api: Add ManagementHandler

--- Api/Handlers/ManagementHandler.php
+++ Api/Handlers/ManagementHandler.php
@@ -14,8 +14,11 @@ namespace Peneus\Api\Handlers;
 
 use \Peneus\Api\Actions\Action;
 use \Peneus\Api\Actions\Management\AddRecordAction;
+use \Peneus\Api\Actions\Management\CreateTableAction;
 use \Peneus\Api\Actions\Management\DeleteRecordAction;
+use \Peneus\Api\Actions\Management\DropTableAction;
 use \Peneus\Api\Actions\Management\EditRecordAction;
+use \Peneus\Api\Actions\Management\ListEntityMappingsAction;
 use \Peneus\Api\Actions\Management\ListRecordsAction;
 use \Peneus\Api\Guards\SessionGuard;
 use \Peneus\Model\Role;
@@ -28,6 +31,12 @@ class ManagementHandler extends Handler
     protected function createAction(string $actionName): ?Action
     {
         return match ($actionName) {
+            'list-entity-mappings' => (new ListEntityMappingsAction)
+                ->AddGuard(new SessionGuard(Role::Admin)),
+            'create-table' => (new CreateTableAction)
+                ->AddGuard(new SessionGuard(Role::Admin)),
+            'drop-table' => (new DropTableAction)
+                ->AddGuard(new SessionGuard(Role::Admin)),
             'list-records' => (new ListRecordsAction)
                 ->AddGuard(new SessionGuard(Role::Admin)),
             'add-record' => (new AddRecordAction)

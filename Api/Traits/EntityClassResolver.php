ea36f7a49c6ffa45f0c82044a5b793fb0c3c28f1
eylemugurel <eylemugurel@hotmail.com>
2025-11-02 07:27:45 +0100

    Add AccountView model entity

--- Api/Traits/EntityClassResolver.php
+++ Api/Traits/EntityClassResolver.php
@@ -14,6 +14,7 @@ namespace Peneus\Api\Traits;
 
 use \Peneus\Model\Account;
 use \Peneus\Model\AccountRole;
+use \Peneus\Model\AccountView;
 use \Peneus\Model\PasswordReset;
 use \Peneus\Model\PendingAccount;
 use \Peneus\Model\PersistentLogin;
@@ -27,6 +28,7 @@ trait EntityClassResolver
     private array $allowedEntityClasses = [
         Account::class,
         AccountRole::class,
+        AccountView::class,
         PasswordReset::class,
         PendingAccount::class,
         PersistentLogin::class,
---
+++ Model/AccountView.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+/**
+ * Account.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Peneus\Model;
+
+class AccountView extends ViewEntity
+{
+    public string $email;
+    public string $displayName;
+    public \DateTime $timeActivated;
+    public ?\DateTime $timeLastLogin;
+    public ?int $role;
+
+    public static function ViewDefinition(): string
+    {
+        return <<<SQL
+        SELECT
+            Account.id,
+            Account.email,
+            Account.displayName,
+            Account.timeActivated,
+            Account.timeLastLogin,
+            AccountRole.role
+        FROM
+            Account
+        LEFT JOIN
+            AccountRole ON AccountRole.accountId = Account.id
+        SQL;
+    }
+}
